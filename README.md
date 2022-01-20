bb36debbf8bc981b0edbf8015a730242ac19f6d0
Abhishek <abhi08as.as@gmail.com>
2022-01-20 22:24:34 +0530

    added streamlit app

--- README.md
+++ README.md
@@ -2,7 +2,18 @@
 
 ## Serving the tensorflow model
 
+You can directly serve the model and make api calls to get the predictions. Pre-requisite is just have docker installed
+
+- Pull the container: `docker pull tensorflow/serving`
+
 - Run the container: `docker run -it -v C:\Users\abhi0\Projects\PGD_thesis:/DryBean -p 8605:8605 --entrypoint /bin/bash tensorflow/serving`
 
 - Inside the container, start the model server: `tensorflow_model_server --rest_api_port=8605 --model_name=dry_bean_model --model_base_path=/DryBean/models/1/`
-```
+
+## Using the app 
+
+- First, serve the model using the above instructions
+
+- Next, in another terminal first install the requirements using: `pip install -r requirements.txt`
+
+- Now just do: `streamlit run deploy\deploy.py`
---
+++ deploy/deploy.py
@@ -0,0 +1,73 @@
+import streamlit as st
+import tensorflow as tf
+import requests
+import numpy as np
+import re
+
+def predict(instances):
+
+    payload = {
+        "instances": [
+            instances
+        ]
+    }
+
+    res = requests.post(
+        url='http://localhost:8605/v1/models/dry_bean_model:predict', 
+        json=payload
+    )
+
+    preds = res.json()
+    return preds
+
+column_names = [
+    'Area',
+    'Perimeter',
+    'MajorAxisLength',
+    'MinorAxisLength',
+    'AspectRation',
+    'Eccentricity',
+    'ConvexArea',
+    'EquivDiameter',
+    'Extent',
+    'Solidity',
+    'roundness',
+    'Compactness',
+    'ShapeFactor1',
+    'ShapeFactor2',
+    'ShapeFactor3',
+    'ShapeFactor4',
+    'Class'
+]
+
+st.sidebar.markdown("<h1><center>Dry bean classifier</center></h1>", unsafe_allow_html=True)
+
+st.sidebar.markdown(f"<p>The values entered follow the columns in the format: <b><i>{column_names}</i><b></p>", unsafe_allow_html=True)
+
+values = st.text_input(
+    f"Enter feature values as comma separated values"
+)
+
+btn = st.button("predict")
+
+if btn:
+    idx2class = {
+        0: 'BARBUNYA',
+        1: 'BOMBAY',
+        2: 'CALI',
+        3: 'DERMASON',
+        4: 'HOROZ',
+        5: 'SEKER',
+        6: 'SIRA'
+    }
+
+    values = [float(v) for v in values.split(',')]
+
+    with st.spinner("Classifying...."):
+        preds = predict(values)
+
+        idx = tf.argmax(preds['predictions'], axis=1)
+
+        st.success(f"The predicted class is: {idx2class[idx.numpy()[0]]}")
+
+
